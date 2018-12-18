80db4fcafbebf377e126b24580a69169c07476a0
savit <thesavit@gmail.com>
2018-12-18 12:10:51 +0900

    updated .gitignore

--- app/models/favorite.rb
+++
@@ -1,3 +0,0 @@
-class Favorite < ApplicationRecord
-    
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :favorites
-
-  get '/favorites/find/:name', to: 'favorites#find'
-
-  get '/key', to: 'favorites#key'
-  
-  root to: "static#index"  
-end
